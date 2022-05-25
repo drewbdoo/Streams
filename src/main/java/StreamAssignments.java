b3394de925cdf09c726bc897ad35ae7057341e9b
Drew <drewbdoo@gmail.com>
2022-05-25 11:47:47 -0400

    limited numbers

--- src/main/java/StreamAssignments.java
+++ src/main/java/StreamAssignments.java
@@ -36,11 +36,30 @@ public class StreamAssignments {
 
 //        //Printing User Inputs assignment
 //        printingUserInputs();
+        
+//        //Limited numbers assignment
+//        limitedNumbers();
 
 
 
 
 
+    }
+
+    private static void limitedNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("Input new numbers, will print out your strings when you enter a negative number");
+        while(true) {
+            int reply = scanner.nextInt();
+            if (reply<0) {
+                break;
+            } else {
+                list.add(reply);
+            }
+        }
+        list.stream().filter(value -> value>1 && value<=5).
+                forEach(System.out::println);
     }
 
     private static void printingUserInputs() {

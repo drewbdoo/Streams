9d5c462c85400ec6d44d25209fae484ba695d865
Drew <drewbdoo@gmail.com>
2022-05-25 10:14:41 -0400

    positive numbers assignment

--- src/main/java/StreamAssignments.java
+++ src/main/java/StreamAssignments.java
@@ -1,12 +1,38 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
-public class AverageOfNumbers {
+public class StreamAssignments {
 
     public static void main(String[] args) {
+//        //Average of Numbers assignment
 //        averageOfNumbers();
-        averageOfSelectedNumbers();
+
+//        //Average of Selected Numbers assignment
+//        averageOfSelectedNumbers();
+
+//        //Positive numbers assignment
+//        List<Integer> list = new ArrayList<>();
+//        list.add(3);
+//        list.add(7);
+//        list.add(4);
+//        list.add(2);
+//        list.add(6);
+//        positive(list);
+
+
+
+    }
+
+
+    public static List<Integer> positive(List<Integer> numbers){
+//        ArrayList<Integer> values = numbers.stream()
+        List<Integer> values = numbers.stream()
+                .filter(value -> value%2==0)
+//                .collect(Collectors.toCollection(ArrayList::new));
+                .collect(Collectors.toList());
+         return values;
     }
 
     private static void averageOfSelectedNumbers() {
